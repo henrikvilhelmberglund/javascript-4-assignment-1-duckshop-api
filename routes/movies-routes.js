9f6a0e3330c6cada282f64fcbca6b920541cd8ae
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-11-02 15:28:36 +0100

    Initial commit

---
+++ routes/movies-routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  listMovies,
+  searchMovies,
+  getMovie,
+} from "../controllers/movies-controller.js";
+
+export const router = express.Router();
+
+// Routes here
+router.route("/list").get(listMovies);
+router.route("/search/:query").get(searchMovies);
+router.route("/:id").get(getMovie);
---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { router } from "./routes/movies-routes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Setting middleware
+app.use(cors());
+// API base URL here
+app.use("/api/v1/movies", router);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
