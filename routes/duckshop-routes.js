5371bfa9c7b020ae23170594a3977a73edcc5c97
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-06 15:27:00 +0100

    feat: add duckshop routes

--- routes/duckshop-routes.js
+++ routes/duckshop-routes.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { getProduct, getProducts } from "../controllers/duckshop-controller.js";
 
 export const router = express.Router();
 
 // Routes here
-// router.route("/list").get(listMovies);
-// router.route("/:id").get(getMovie);
+router.route("/products").get(getProducts);
+router.route("/product/:id").get(getProduct);
