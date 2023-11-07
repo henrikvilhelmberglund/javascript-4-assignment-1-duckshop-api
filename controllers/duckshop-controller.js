139ba2910450c273504847d1add90de8df7d4490
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-07 09:48:15 +0100

    fix: remove unused endpoint

--- controllers/duckshop-controller.js
+++ controllers/duckshop-controller.js
@@ -1,12 +1,12 @@
 import { myClient } from "../utils/myClient.js";
 
 export async function getProducts(req, res) {
-  const response = await myClient("products");
+  const response = await myClient();
   res.status(response.statusCode).json(response);
 }
 
 export async function getProduct(req, res) {
   const id = req.params.id !== undefined ? req.params.id : "error";
-  const response = await myClient("product", id);
+  const response = await myClient(id);
   res.status(response.statusCode).json(response);
 }
--- utils/myClient.js
+++ utils/myClient.js
@@ -1,10 +1,9 @@
-import fetch from "node-fetch";
 import fs from "fs";
 // hvb-console is my fun library for adding color to console logs and writing out line numbers for them
 // replaces the normal console.log etc so remove it if necessary
 import console from "hvb-console";
 
-export async function myClient(endpoint, id) {
+export async function myClient(id) {
   const response = {
     statusCode: 200,
     message: "Success",
