c5f2579321fdeda21257d769a7021570ca877d95
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-07 09:41:06 +0100

    fix: put mockData in response object, set mockData to undefined if not found

--- utils/myClient.js
+++ utils/myClient.js
@@ -8,22 +8,24 @@ export async function myClient(endpoint, id) {
   const response = {
     statusCode: 200,
     message: "Success",
+    mockData: undefined,
   };
 
-  let mockData = JSON.parse(fs.readFileSync("mock.json", "utf-8"));
+  response.mockData = JSON.parse(fs.readFileSync("mock.json", "utf-8"));
 
   if (id) {
     console.log(id);
-    mockData = mockData.filter((product) => {
+    response.mockData = response.mockData.filter((product) => {
       if (+id === product.id) {
         return true;
       }
     });
     // don't want an array here
-    mockData = mockData[0];
-    if (mockData.length === 0) {
+    response.mockData = response.mockData[0];
+    if (!response.mockData) {
       response.statusCode = 404;
       response.message = `Product with id ${id} not found`;
+      response.mockData = undefined;
     }
   }
   // url = `${process.env.BASE_URL}${process.env.API_BASE}${endpoint}`;
@@ -40,5 +42,5 @@ export async function myClient(endpoint, id) {
   // const data = await response.json();
   // console.log(response);
   // console.log(mockData);
-  return { ...response, mockData };
+  return response;
 }
