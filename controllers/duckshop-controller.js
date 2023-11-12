6d45ef6b11b20511071a7f8d9aa45f1bece62df1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-12 22:00:38 +0100

    docs: add comment

--- controllers/duckshop-controller.js
+++ controllers/duckshop-controller.js
@@ -5,6 +5,7 @@ export async function getProducts(req, res) {
   res.status(response.statusCode).json(response);
 }
 
+// använder bara getProducts() i frontend för då slipper jag fetcha för varje produkt, räcker med en gång!
 export async function getProduct(req, res) {
   const id = req.params.id !== undefined ? req.params.id : "error";
   const response = await myClient(id);
